db82cdd1ba9291734350943e036f8c64369830f0
nd-jeremias <nd.jeremias@live.com>
2025-11-29 19:19:06 -0300

    Agregado Relaciones M:M (Verificar)

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,16 +3,22 @@ import { Ventas, DetalleVentas, Productos, Categorias, Libros, Discos, Generos, 
 
 // Definir Relaciones
 
-// Relaciones DetalleVenta → Venta y Producto
+// --- Relación de muchos a muchos: Ventas <-> Productos a través de DetalleVentas ---
+Ventas.belongsToMany(Productos, { through: DetalleVentas, foreignKey: 'id_venta', otherKey: 'id_producto', as: 'productos_vendidos' });
+Productos.belongsToMany(Ventas, { through: DetalleVentas, foreignKey: 'id_producto', otherKey: 'id_venta', as: 'ventas_incluidas' });
+
+// --- Relacion 1 a muchos: DetalleVenta → Venta y Producto - (Se mantienen para permitir el acceso directo a los atributos de DetalleVentas (cantidad, precio_unitario)) ---
 Ventas.hasMany(DetalleVentas, { foreignKey: 'id_venta', as: 'detalle' });
 DetalleVentas.belongsTo(Ventas, { foreignKey: 'id_venta', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-Productos.hasMany(DetalleVentas, { foreignKey: 'id_producto' });
+Productos.hasMany(DetalleVentas, { foreignKey: 'id_producto', as: 'detalles_venta' });
 DetalleVentas.belongsTo(Productos, { foreignKey: 'id_producto', as: 'producto', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-// Relación: productos.category → categorias.id
-Categorias.hasMany(Productos, { foreignKey: 'id_categoria' });
+// --- Relación 1 a muchos: productos.category → categorias.id () ---
+Categorias.hasMany(Productos, { foreignKey: 'id_categoria', as: 'productos' });
 Productos.belongsTo(Categorias, { foreignKey: 'id_categoria', as: 'categoria' });
+
+// --- Relación 1 a 1 ---
 
 // Relación: libros.id_producto → productos.id
 Productos.hasOne(Libros, { foreignKey: 'id_producto', as: 'info_libro' });
@@ -22,12 +28,14 @@ Libros.belongsTo(Productos, { foreignKey: 'id_producto', onDelete: 'CASCADE', on
 Productos.hasOne(Discos, { foreignKey: 'id_producto', as: 'info_disco' });
 Discos.belongsTo(Productos, { foreignKey: 'id_producto', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
-// Relación: Productos → Generos
-Discos.belongsTo(Generos, { foreignKey: 'id_genero', as: 'genero'});
-Generos.hasMany(Discos, { foreignKey: 'id_genero', as: 'disco'});
-Libros.belongsTo(Generos, { foreignKey: 'id_genero', as: 'genero'});
-Generos.hasMany(Libros, { foreignKey: 'id_genero', as: 'libro'});
+// --- Relación 1 a muchos: Productos → Generos ---
+
+// Relación: Libros/Discos a Generos
+Discos.belongsTo(Generos, { foreignKey: 'id_genero', as: 'genero' });
+Generos.hasMany(Discos, { foreignKey: 'id_genero', as: 'discos' }); 
+
+Libros.belongsTo(Generos, { foreignKey: 'id_genero', as: 'genero' });
+Generos.hasMany(Libros, { foreignKey: 'id_genero', as: 'libros' }); 
 
 // exportar las relaciones hechas
-
 export { Ventas, DetalleVentas, Productos, Categorias, Libros, Discos, Generos, Usuarios };
